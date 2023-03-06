13ac061fd486bcb3597006027fb01cc796c455f9
kenji <kenji@email.com>
2023-03-06 13:27:03 +0000

    remember-me実装

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,21 +10,34 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
   
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
   
+  
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id:user_id)
+      @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id:user_id)
-      user && user.authenticated?(cookies[:remember_token])
-      log_in user
-      @current_user = user
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
+
+  
+  
   
   def logged_in?
     !current_user.nil?
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
   
   def User.digest(string)
     cost=
-      if ActiveRecord::SecurePassword.min_cost
+      if ActiveModel::SecurePassword.min_cost
         BCrypt::Engine::MIN_COST
       else
         BCrypt::Engine.cost
@@ -33,10 +33,18 @@ class User < ApplicationRecord
   
   def remember
     self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.new_token(remember_token))
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
   
-  def authenticated?(rememner_token)
-    BCrypt::Password.new(rememner_digest).is_password?(remember_token)
+  
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+  
+  
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+  
 end
