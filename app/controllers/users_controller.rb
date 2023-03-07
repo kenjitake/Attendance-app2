4de4285480610125444ca29e41c5a09b970e323a
kenji <kenji@email.com>
2023-03-07 09:19:27 +0000

    users#index一部実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
   
   before_action :set_user, only:[:show, :edit, :update]
-  before_action :logged_in_user, only:[:show, :edit, :update]
+  before_action :logged_in_user, only:[:show, :index, :edit, :update]
   before_action :correct_user, only:[:edit, :update]
   
   def show
-    @user = User.find(params[:id])
+    
   end
   
   
-  def new
+  def new 
     @user = User.new
   end
   
@@ -25,14 +25,14 @@ class UsersController < ApplicationController
   end
   
   def index
+    @users = User.all
   end
   
   def edit
-    @user = User.find(params[:id])
+    
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success]= "ユーザー情報を更新しました"
       redirect_to @user
@@ -55,13 +55,13 @@ class UsersController < ApplicationController
     
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger]= "ログインしてください"
         redirect_to login_url
       end
     end
     
     def correct_user
-      @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -36,10 +36,20 @@ module SessionsHelper
     end
   end
 
-  
-  
-  
   def logged_in?
     !current_user.nil?
+  end
+  
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def redirect_back_or(default_url)
+    redirect_to(session[:forwarding_url] || default_url)
+    session.delete(:forwarding_url)
+  end
+  
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
