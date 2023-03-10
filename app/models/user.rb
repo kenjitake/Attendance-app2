b8f5d5643edd3107905fd309d0224e16f5b6e353
kenji <kenji@email.com>
2023-03-10 09:51:52 +0000

    basic,work_timeカラムを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,9 @@ class User < ApplicationRecord
            
   validates:department, length: { in: 2..30 }, allow_blank:true
   
+  validates:work_time, presence:true
+  validates:basic_time, presence:true
+  
   has_secure_password
   validates:password, presence:true, length: { minimum: 6 }, allow_nil:true
   
