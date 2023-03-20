a7463db985b11a2a92c46f7eb56b0889e94cedd4
kenji <kenji@email.com>
2023-03-20 06:11:14 +0000

    勤怠編集の更新実装

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,5 +1,5 @@
 class AttendancesController < ApplicationController
-  before_action :set_user, only: :edit_one_month
+  before_action :set_user, only: [:edit_one_month, :update_one_month]
   before_action :logged_in_user, only: [:update, :edit_one_month]
   before_action :set_one_month, only: :edit_one_month
   
@@ -28,4 +28,26 @@ class AttendancesController < ApplicationController
   
   def edit_one_month
   end
+  
+  def update_one_month
+    ActiveRecord::Base.transaction do
+      attendance_params.each do |id, item|
+        attendance = Attendance.find(id)
+        attendance.update_attributes!(item)
+      end
+    end
+    flash[:success]= "勤怠情報を編集しました"
+    redirect_to user_url(date: params[:date])
+  rescue ActiveRecord::RecordInvalid
+    flash[:danger]= "無効な入力があった為、更新をキャンセルしました"
+    redirect_to attendances_edit_one_month_user_url(date: params[:date])
+  end
+  
+  
+  private
+   
+   
+    def attendance_params
+      params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
+    end
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -6,7 +6,15 @@ class Attendance < ApplicationRecord
   
   validate:finished_at_is_invalid_without_a_started_at
   
+  validate:finished_at_faster_than_started_at_is_invalid
+  
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
+  end
+  
+  def finished_at_faster_than_started_at_is_invalid
+    if started_at.present? && finished_at.present?
+      errors.add(:started_at, "より早い退勤打刻は無効です") if started_at > finished_at
+    end
   end
 end
